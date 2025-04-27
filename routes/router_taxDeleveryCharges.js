7775148593f561ff0ae08d528e1029e3f2ea745c
alone-02 <santoshbhaskal2127@gmail.com>
2025-04-27 22:06:16 +0530

    custom idol functionality

--- routes/router_taxDeleveryCharges.js
+++ routes/router_taxDeleveryCharges.js
@@ -1,12 +1,14 @@
-// routes/taxDeliveryRoutes.js
 const express = require("express");
 const taxDeliveryChargesController = require("../controllers/taxDeliveryChargesController");
 
 const router = express.Router();
 
 router.post("/add", taxDeliveryChargesController.addTaxDeliveryCharges);
+
 router.get("/fetch", taxDeliveryChargesController.fetchTaxDeliveryCharges);
+
 router.put("/update/:id", taxDeliveryChargesController.updateTaxDeliveryCharges);
+
 router.delete("/delete/:id", taxDeliveryChargesController.deleteTaxDeliveryCharges);
 
 module.exports = router;
